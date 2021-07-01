db86765484581078fb9b67a66a480238cadb7efb
jeong <hi_coffeeci@likelion.org>
2021-07-01 12:40:07 +0900

    7/1 6ban class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# 단순 view 만들기
+# MVT 중 V
+# def hello_world(request):
+#     return HttpResponse('Hello World!') return 부분 변경
+
 def hello_world(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'base.html')
