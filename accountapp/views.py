ddb3d7e50ebca2d5492dcfb06e16c632bc3ba33a
jeong <hi_coffeeci@likelion.org>
2021-07-08 12:40:24 +0900

    7/8 class 6ban commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 # 단순 view 만들기
@@ -9,4 +10,7 @@ from django.shortcuts import render
 #     return HttpResponse('Hello World!') return 부분 변경
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text':'POST METHOD!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text':'POST METHOD!'})
