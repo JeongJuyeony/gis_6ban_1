0d728f7a8464657e63aa5be04ccb018defea5ca1
jeong <hi_coffeeci@likelion.org>
2021-07-22 12:43:37 +0900

    7/22 class 6ban commit 4

--- gis_6ban_1/settings.py
+++ gis_6ban_1/settings.py
@@ -55,7 +55,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',  # 외부라이브러리 bootstrap4를 사용하겠다.
-    'accountapp'   # acoountapp을 사용하겠다고 명시
+    'accountapp',   # acoountapp을 사용하겠다고 명시
+    'profileapp'
 ]
 
 MIDDLEWARE = [
