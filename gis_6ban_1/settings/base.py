61bbc463e1b34ff909a523c5b4c54e2cea12e0ab
jeong <hi_coffeeci@likelion.org>
2021-09-13 12:43:58 +0900

    9/13 class 6ban_1 commit 1

--- gis_6ban_1/settings/base.py
+++ gis_6ban_1/settings/base.py
@@ -11,28 +11,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.contrib import messages
 from django.urls import reverse_lazy
-BASE_DIR = Path(__file__).resolve().parent.parent
-# settings.py 파일의 부모의 부모 경로를 BASE_DIR에 저장. 즉, 최상위 gis_6ban_1 폴더를 지칭
-env_list = dict()
-local_env = open(os.path.join(BASE_DIR, '.env'), encoding='utf-8') # 불러들이기
-# 운영체제의 경로탐색에서 BASE_DIR과 env 파일을 join 즉, 최상위 폴더 gis_6ban_1에 있는 env 파일을 oepn
-while True:
-    line = local_env.readline() # 라인 한줄씩 일기
-    if not line: # 라인이 없다면 break
-        break
-    line = line.replace('\n', '')
-    start = line.find('=') # =의 위치를 판단하고 인덱스 알려준 후 start에 저장
-    key = line[:start] # 시크릿 키에서 =을 기준으로 왼쪽을 key 오른쪽을 value로 판단
-    value = line[start+1:]
-    env_list[key] = value
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# base.py 파일의 부모의 부모 경로를 BASE_DIR에 저장. 즉, 최상위 gis_6ban_1 폴더를 지칭
+
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
@@ -83,14 +65,8 @@ TEMPLATES = [
     },
 ]
 WSGI_APPLICATION = 'gis_6ban_1.wsgi.application'
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
